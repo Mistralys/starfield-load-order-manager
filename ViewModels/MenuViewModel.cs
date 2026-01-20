62b90c843e60575bdc7da1862c7f5ec51e83fdc1
Mistralys <s.mordziol@gmail.com>
2026-01-20 07:59:22 +0100

    Using the translations.

--- ViewModels/MenuViewModel.cs
+++ ViewModels/MenuViewModel.cs
@@ -9,48 +9,46 @@ namespace LoadOrderKeeper.ViewModels
     public class MenuViewModel
     {
         // Window Title
-        public string WindowTitle => $"Starfield Load Order Keeper v{VersionService.GetApplicationVersion()}";
+        public string WindowTitle => string.Format(Resources.MainWindowResources.WindowTitleFormat, VersionService.GetApplicationVersion());
 
         // File Menu
-        public string FileMenuHeader { get; } = "_File";
-        public string OpenPluginsMenuText { get; } = "Open _Plugins.txt";
-        public string OpenReferenceMenuText { get; } = "Open _Reference File";
-        public string OpenAppDataFolderMenuText { get; } = "Open _AppData Folder";
-        public string OpenGameFolderMenuText { get; } = "Open _Game Folder";
-        public string ExitMenuText { get; } = "E_xit";
+        public string FileMenuHeader => Resources.MainWindowResources.FileMenuHeader;
+        public string OpenPluginsMenuText => Resources.MainWindowResources.OpenPluginsMenuText;
+        public string OpenReferenceMenuText => Resources.MainWindowResources.OpenReferenceMenuText;
+        public string OpenAppDataFolderMenuText => Resources.MainWindowResources.OpenAppDataFolderMenuText;
+        public string OpenGameFolderMenuText => Resources.MainWindowResources.OpenGameFolderMenuText;
+        public string ExitMenuText => Resources.MainWindowResources.ExitMenuText;
 
         // Edit Menu
-        public string EditMenuHeader { get; } = "_Edit";
-        public string SettingsMenuText { get; } = "_Settings...";
+        public string EditMenuHeader { get; } = Resources.MainWindowResources.EditMenuHeader;
+        public string SettingsMenuText { get; } = Resources.MainWindowResources.SettingsMenuText;
 
         // Profile Menu
-        public string ProfileMenuHeader { get; } = "_Profile";
-        public string SwitchProfileMenuText { get; } = "_Switch Profile...";
-        public string ManageProfilesMenuText { get; } = "_Manage Profiles...";
-        public string ReferenceHistoryMenuText { get; } = "History of changes...";
-        public string ViewPendingChangesMenuText { get; } = "_View Pending Changes...";
+        public string ProfileMenuHeader { get; } = Resources.MainWindowResources.ProfileMenuHeader;
+        public string SwitchProfileMenuText { get; } = Resources.MainWindowResources.SwitchProfileMenuText;
+        public string ManageProfilesMenuText { get; } = Resources.MainWindowResources.ManageProfilesMenuText;
+        public string ReferenceHistoryMenuText { get; } = Resources.MainWindowResources.ReferenceHistoryMenuText;
+        public string ViewPendingChangesMenuText { get; } = Resources.MainWindowResources.ViewPendingChangesMenuText;
 
         // Help Menu
-        public string HelpMenuHeader { get; } = "_Help";
-        public string CheckForUpdatesMenuText { get; } = "Check for _Updates...";
-        public string AboutMenuText { get; } = "_About...";
+        public string HelpMenuHeader { get; } = Resources.MainWindowResources.HelpMenuHeader;
+        public string CheckForUpdatesMenuText { get; } = Resources.MainWindowResources.CheckForUpdatesMenuText;
+        public string AboutMenuText { get; } = Resources.MainWindowResources.AboutMenuText;
 
         // Debug Menu
-        public string DebugMenuHeader { get; } = "_Debug";
-        public string ResetConfigMenuText { get; } = "_Reset Configuration (for testing)...";
-        public string OpenConfigFolderMenuText { get; } = "Open _Configuration Folder";
-        public string ThrowTestExceptionMenuText { get; } = "_Throw Test Exception";
+        public string DebugMenuHeader { get; } = Resources.MainWindowResources.DebugMenuHeader;
+        public string ResetConfigMenuText { get; } = Resources.MainWindowResources.ResetConfigMenuText;
+        public string OpenConfigFolderMenuText { get; } = Resources.MainWindowResources.OpenConfigFolderMenuText;
+        public string ThrowTestExceptionMenuText { get; } = Resources.MainWindowResources.ThrowTestExceptionMenuText;
 
         // Button Text
-        public string DownloadOptionsButtonText { get; } = "Download options...";
+        public string DownloadOptionsButtonText { get; } = Resources.MainWindowResources.DownloadOptionsButtonText;
 
         // Labels
-        public string CurrentTargetLabel { get; } = "Current Plugins.txt target:";
-        public string TargetPrefixText { get; } = "Target: ";
-        public string ActiveProfilePrefixText { get; } = "Active Profile: ";
-        public string RecentStatusMessagesText { get; } = "Recent Status Messages:";
+        public string ActiveProfilePrefixText { get; } = Resources.MainWindowResources.ActiveProfilePrefixText;
+        public string RecentStatusMessagesText { get; } = Resources.MainWindowResources.RecentStatusMessagesText;
 
         // Warning Messages
-        public string PluginsModifiedWarningText { get; } = "Plugins.txt was modified outside Load Order Keeper.";
+        public string PluginsModifiedWarningText { get; } = Resources.MainWindowResources.PluginsModifiedWarningText;
     }
 }
